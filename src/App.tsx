6b3ebc4e64dd8c1c37f03aa2dcf0ea0f856fa4a7
Harikrishnan KR <harikrishnankr@Harikrishnans-MacBook-Pro-2.local>
2024-09-18 12:11:16 +0530

    Error Boundaries

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 import KanbanBoard from "./pages/Kanban";
 import "./styles.css";
+import ErrorBoundary from "./components/ErrorBoundary";
 
 const App: React.FC = () => {
   return (
     <div className="App">
-      <KanbanBoard />
+      <ErrorBoundary>
+        <KanbanBoard />
+      </ErrorBoundary>
     </div>
   );
 }
